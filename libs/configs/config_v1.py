bc08f9467b31066132830a2b4f9a7e5deb7dcf79
CharlesShang <shangchong90@gmail.com>
2017-04-21 01:54:53 +0800

    momentum

--- libs/configs/config_v1.py
+++ libs/configs/config_v1.py
@@ -45,7 +45,7 @@ tf.app.flags.DEFINE_float(
     'weight_decay', 0.00004, 'The weight decay on the model weights.')
 
 tf.app.flags.DEFINE_string(
-    'optimizer', 'rmsprop',
+    'optimizer', 'momentum',
     'The name of the optimizer, one of "adadelta", "adagrad", "adam",'
     '"ftrl", "momentum", "sgd" or "rmsprop".')
 
