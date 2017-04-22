191c17101e211841376b3d4a68db2cccf7370250
CharlesShang <shangchong90@gmail.com>
2017-04-22 23:25:15 +0800

    wrap the training functions

---
+++ train/train.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+ 
+import functools
+import os, sys
+import time
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from time import gmtime, strftime
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+import libs.configs.config_v1 as cfg
+import libs.datasets.dataset_factory as datasets
+import libs.nets.nets_factory as network 
+
+import libs.preprocessings.coco_v1 as coco_preprocess
+import libs.nets.pyramid_network as pyramid_network
+import libs.nets.resnet_v1 as resnet_v1
+
+from train.foo import foo
+
+from train.train_utils import _configure_learning_rate, _configure_optimizer, \
+  _get_variables_to_train, _get_init_fn, get_var_list_to_restore
+
+resnet50 = resnet_v1.resnet_v1_50
+FLAGS = tf.app.flags.FLAGS
+
+def solve(global_step):
+    """add solver to losses"""
+    # learning reate
+    lr = _configure_learning_rate(82783, global_step)
+    optimizer = _configure_optimizer(lr)
+    tf.summary.scalar('learning_rate', lr)
+
+    # compute and apply gradient
+    losses = tf.get_collection(tf.GraphKeys.LOSSES)
+    regular_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+    regular_loss = tf.add_n(regular_losses)
+    out_loss = tf.add_n(losses)
+    total_loss = tf.add_n(losses + regular_losses)
+
+    tf.summary.scalar('total_loss', total_loss)
+    tf.summary.scalar('out_loss', out_loss)
+    tf.summary.scalar('regular_loss', regular_loss)
+
+    update_ops = []
+    variables_to_train = _get_variables_to_train()
+    # update_op = optimizer.minimize(total_loss)
+    gradients = optimizer.compute_gradients(total_loss, var_list=variables_to_train)
+    grad_updates = optimizer.apply_gradients(gradients, 
+            global_step=global_step)
+    update_ops.append(grad_updates)
+    
+    # update moving mean and variance
+    if FLAGS.update_bn:
+        update_bns = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        update_bn = tf.group(*update_bns)
+        update_ops.append(update_bn)
+
+    return tf.group(*update_ops)
+
+def restore(sess):
+     """choose which param to restore"""
+     if FLAGS.pretrained_model:
+        if tf.gfile.IsDirectory(FLAGS.pretrained_model):
+            checkpoint_path = tf.train.latest_checkpoint(FLAGS.pretrained_model)
+        else:
+            checkpoint_path = FLAGS.pretrained_model
+
+        if FLAGS.checkpoint_exclude_scopes is None:
+            FLAGS.checkpoint_exclude_scopes='pyramid'
+        if FLAGS.checkpoint_include_scopes is None:
+            FLAGS.checkpoint_include_scopes='resnet_v1_50'
+
+        vars_to_restore = get_var_list_to_restore()
+        for var in vars_to_restore:
+            print ('restoring ', var.name)
+      
+        try:
+           restorer = tf.train.Saver(vars_to_restore)
+           restorer.restore(sess, checkpoint_path)
+           print ('Restored %d(%d) vars from %s' %(
+               len(vars_to_restore), len(tf.global_variables()),
+               checkpoint_path ))
+        except:
+           print ('Checking your params %s' %(checkpoint_path))
+           raise
+    
+def train():
+    """The main function that runs training"""
+
+    ## data
+    image, ih, iw, gt_boxes, gt_masks, num_instances, img_id = \
+        datasets.get_dataset(FLAGS.dataset_name, 
+                             FLAGS.dataset_split_name, 
+                             FLAGS.dataset_dir, 
+                             FLAGS.im_batch,
+                             is_training=True)
+    
+    ## network
+    logits, end_points, pyramid_map = network.get_network(FLAGS.network, image,
+            weight_decay=FLAGS.weight_decay)
+    outputs = pyramid_network.build(end_points, ih, iw, pyramid_map, 
+            num_classes=81,
+            base_anchors=9,
+            is_training=True,
+            gt_boxes=gt_boxes, gt_masks=gt_masks,
+            loss_weights=[0.1, 0.2, 1.0, 0.1, 0.5])
+
+
+    total_loss = outputs['total_loss'] 
+    losses  = outputs['losses']
+    batch_info = outputs['batch_info']
+    regular_loss = tf.add_n(tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES))
+
+    ## solvers
+    global_step = slim.create_global_step()
+    update_op = solve(global_step)
+
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.8)
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+    init_op = tf.group(
+            tf.global_variables_initializer(),
+            tf.local_variables_initializer()
+            )
+    sess.run(init_op)
+
+    summary_op = tf.summary.merge_all()
+    logdir = os.path.join(FLAGS.train_dir, strftime('%Y%m%d%H%M%S', gmtime()))
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+    summary_writer = tf.summary.FileWriter(logdir, graph=sess.graph)
+
+    ## restore
+    restore(sess)
+
+    ## main loop
+    coord = tf.train.Coordinator()
+    tf.train.start_queue_runners(sess=sess, coord=coord)
+    saver = tf.train.Saver(max_to_keep=20)
+
+    for step in range(FLAGS.max_iters):
+        
+        start_time = time.time()
+
+        s_, tot_loss, reg_lossnp, img_id_str, \
+        rpn_box_loss, rpn_cls_loss, refined_box_loss, refined_cls_loss, mask_loss, \
+        gt_boxesnp, \
+        rpn_batch_pos, rpn_batch, refine_batch_pos, refine_batch, mask_batch_pos, mask_batch = \
+                     sess.run([update_op, total_loss, regular_loss, img_id] + 
+                              losses + 
+                              [gt_boxes] + 
+                              batch_info)
+        
+        duration_time = time.time() - start_time
+        if step % 1 == 0: 
+            print ( """iter %d: image-id:%07d, time:%.3f(sec), regular_loss: %.6f, """
+                    """total-loss %.4f(%.4f, %.4f, %.6f, %.4f, %.4f), """
+                    """instances: %d, """
+                    """batch:(%d|%d, %d|%d, %d|%d)""" 
+                   % (step, img_id_str, duration_time, reg_lossnp, 
+                      tot_loss, rpn_box_loss, rpn_cls_loss, refined_box_loss, refined_cls_loss, mask_loss,
+                      gt_boxesnp.shape[0], 
+                      rpn_batch_pos, rpn_batch, refine_batch_pos, refine_batch, mask_batch_pos, mask_batch))
+
+            if np.isnan(tot_loss) or np.isinf(tot_loss):
+                print (gt_boxesnp)
+                raise
+          
+        if step % 100 == 0:
+            summary_str = sess.run(summary_op)
+            summary_writer.add_summary(summary_str, step)
+
+        if (step % 1000 == 0 or step + 1 == FLAGS.max_iters) and step != 0:
+            checkpoint_path = os.path.join(FLAGS.train_dir, 
+                                           FLAGS.dataset_name + '_model.ckpt')
+            saver.save(sess, checkpoint_path, global_step=step)
+
+        if coord.should_stop():
+            coord.request_stop()
+            coord.join(threads)
+
+
+if __name__ == '__main__':
+    train()
