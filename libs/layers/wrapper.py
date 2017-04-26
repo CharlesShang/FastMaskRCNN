394b8055ab5f2ccc19ab976ae8b4cce14ae0bccc
CharlesShang <shangchong90@gmail.com>
2017-04-27 00:53:14 +0800

    change assign api

--- libs/layers/wrapper.py
+++ libs/layers/wrapper.py
@@ -149,7 +149,7 @@ def gen_all_anchors(height, width, stride, scales, scope='GenAnchors'):
     
     return all_anchors
 
-def assign_boxes(gt_boxes, layers, scope='AssignGTBoxes'):
+def assign_boxes(gt_boxes, tensors, layers, scope='AssignGTBoxes'):
 
     with tf.name_scope(scope) as sc:
         min_k = layers[0]
@@ -159,11 +159,15 @@ def assign_boxes(gt_boxes, layers, scope='AssignGTBoxes'):
                      [ gt_boxes, min_k, max_k ],
                      tf.int32)
         assigned_layers = tf.reshape(assigned_layers, [-1])
-        assigned_gt_boxes = []
-        for t in layers:
-            t = tf.cast(t, tf.int32)
-            inds = tf.where(tf.equal(assigned_layers, t))
-            inds = tf.reshape(inds, [-1])
-            assigned_gt_boxes.append(tf.gather(gt_boxes, inds))
 
-        return assigned_gt_boxes + [assigned_layers]
+        assigned_tensors = []
+        for t in tensors:
+            split_tensors = []
+            for l in layers:
+                tf.cast(l, tf.int32)
+                inds = tf.where(tf.equal(assigned_layers, l))
+                inds = tf.reshape(inds, [-1])
+                split_tensors.append(tf.gather(t, inds))
+            assigned_tensors.append(split_tensors)
+
+        return assigned_tensors + [assigned_layers]
