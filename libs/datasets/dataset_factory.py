changed anchor to match with MaskRCNN original paper fixed the issue that is_training=False causes no bounding boxes during test fixed batch normalization gradient update

libs/datasets/dataset_factory.py
@@ -16,7 +16,8 @@ def get_dataset(dataset_name, split_name, dataset_dir,
         file_pattern = dataset_name + '_' + split_name + '*.tfrecord' 
 
     tfrecords = glob.glob(dataset_dir + '/records/' + file_pattern)
-    image, ih, iw, gt_boxes, gt_masks, num_instances, img_id = coco.read(tfrecords)
+    print(tfrecords)
+    image, ih, iw, gt_boxes, gt_masks, num_instances, img_id = coco.read(tfrecords, is_training=is_training)
 
     image, gt_boxes, gt_masks = coco_preprocess.preprocess_image(image, gt_boxes, gt_masks, is_training)
     #visualize_input(gt_boxes, image, tf.expand_dims(gt_masks, axis=3))

libs/layers/sample.py
@@ -13,7 +13,7 @@ from libs.logs.log import LOG
 
 _DEBUG=False
 
-def sample_rpn_outputs(boxes, scores, indexs, is_training=False, only_positive=False):
+def sample_rpn_outputs(boxes, scores, indexs, is_training=False, only_positive=False, with_nms=False):
   """Sample boxes according to scores and some learning strategies
   assuming the first class is background
   Params:
@@ -50,7 +50,8 @@ def sample_rpn_outputs(boxes, scores, indexs, is_training=False, only_positive=F
   scores = scores[keeps]
   indexs = indexs[keeps]
   
-  # filter with scores
+
+  # filter with nms
   order = scores.ravel().argsort()[::-1]
   if pre_nms_top_n > 0:
     order = order[:pre_nms_top_n]
@@ -58,15 +59,16 @@ def sample_rpn_outputs(boxes, scores, indexs, is_training=False, only_positive=F
   scores = scores[order]
   indexs = indexs[order]
 
-  # filter with nms
-  det = np.hstack((boxes, scores)).astype(np.float32)
-  keeps = nms_wrapper.nms(det, rpn_nms_threshold)
-  
+  if with_nms is True:
+    det = np.hstack((boxes, scores)).astype(np.float32)
+    keeps = nms_wrapper.nms(det, rpn_nms_threshold)
+
   if post_nms_top_n > 0:
     keeps = keeps[:post_nms_top_n]
   boxes = boxes[keeps, :]
   scores = scores[keeps].astype(np.float32)
   indexs = indexs[keeps]
+
   batch_inds = np.zeros([boxes.shape[0]], dtype=np.int32)
 
   # # random sample boxes
@@ -86,7 +88,7 @@ def sample_rpn_outputs(boxes, scores, indexs, is_training=False, only_positive=F
 
 def sample_rpn_outputs_wrt_gt_boxes(boxes, scores, gt_boxes, indexs, is_training=False, only_positive=False):
     """sample boxes for refined output"""
-    boxes, scores, batch_inds, indexs = sample_rpn_outputs(boxes, scores, indexs, is_training, only_positive)
+    boxes, scores, batch_inds, indexs = sample_rpn_outputs(boxes, scores, indexs, is_training=is_training, only_positive=only_positive, with_nms=True)
 
     if gt_boxes.size > 0:
         overlaps = cython_bbox.bbox_overlaps(
@@ -111,7 +113,7 @@ def sample_rpn_outputs_wrt_gt_boxes(boxes, scores, gt_boxes, indexs, is_training
         
         # TODO: sampling strategy
         bg_inds = np.where((max_overlaps < cfg.FLAGS.bg_threshold))[0]
-        bg_rois = max(min(cfg.FLAGS.rois_per_image - fg_rois, fg_rois * 3), 128)#64
+        bg_rois = int(max(min(cfg.FLAGS.rois_per_image - fg_rois, fg_rois * 3),  cfg.FLAGS.rois_per_image * cfg.FLAGS.fg_roi_fraction))#128
         if bg_inds.size > 0 and bg_rois < bg_inds.size:
             bg_inds = np.random.choice(bg_inds, size=bg_rois, replace=False)
 
@@ -120,7 +122,7 @@ def sample_rpn_outputs_wrt_gt_boxes(boxes, scores, gt_boxes, indexs, is_training
             mask_fg_inds = keep_inds
     else:
         bg_inds = np.arange(boxes.shape[0])
-        bg_rois = min(int(cfg.FLAGS.rois_per_image * (1-cfg.FLAGS.fg_roi_fraction)), 128)#64
+        bg_rois = int(min(cfg.FLAGS.rois_per_image * (1-cfg.FLAGS.fg_roi_fraction),  cfg.FLAGS.rois_per_image * cfg.FLAGS.fg_roi_fraction))#128
         if bg_rois < bg_inds.size:
             bg_inds = np.random.choice(bg_inds, size=bg_rois, replace=False)
 

libs/nets/pyramid_network.py
@@ -269,7 +269,7 @@ def build_heads(pyramid, ih, iw, num_classes, base_anchors, is_training=False, g
         if is_training is True:
           ### for training, rcnn and maskrcnn take rpn boxes as inputs
           rpn_rois_to_rcnn, rpn_scores_to_rcnn, rpn_batch_inds_to_rcnn, rpn_indexs_to_rcnn, rpn_rois_to_mask, rpn_scores_to_mask, rpn_batch_inds_to_mask, rpn_indexs_to_mask = \
-                sample_rpn_outputs_with_gt(rpn_final_boxes, rpn_final_scores, gt_boxes, indexs, is_training=is_training, only_positive=True)
+                sample_rpn_outputs_with_gt(rpn_final_boxes, rpn_final_scores, gt_boxes, indexs, is_training=is_training, only_positive=False)
           # rcnn_rois, rcnn_scores, rcnn_batch_inds, rcnn_indexs, mask_rois, mask_scores, mask_batch_inds, mask_indexs = \
           #       sample_rpn_outputs_with_gt(rpn_final_boxes, rpn_final_scores, gt_boxes, indexs, is_training=is_training, only_positive=True)
         else:
@@ -303,14 +303,14 @@ def build_heads(pyramid, ih, iw, num_classes, base_anchors, is_training=False, g
         # to 7 x 7
         rcnn = slim.max_pool2d(rcnn_cropped_features, [3, 3], stride=2, padding='SAME')
         rcnn = slim.flatten(rcnn)
-        rcnn = slim.fully_connected(rcnn, 1024, activation_fn=tf.nn.relu)
+        rcnn = slim.fully_connected(rcnn, 1024, activation_fn=tf.nn.relu, weights_initializer=tf.truncated_normal_initializer(stddev=0.001))
         rcnn = slim.dropout(rcnn, keep_prob=0.75, is_training=is_training)
-        rcnn = slim.fully_connected(rcnn,  1024, activation_fn=tf.nn.relu)
+        rcnn = slim.fully_connected(rcnn,  1024, activation_fn=tf.nn.relu, weights_initializer=tf.truncated_normal_initializer(stddev=0.001))
         rcnn = slim.dropout(rcnn, keep_prob=0.75, is_training=is_training)
         rcnn_clses = slim.fully_connected(rcnn, num_classes, activation_fn=None, normalizer_fn=None, 
-                weights_initializer=tf.truncated_normal_initializer(stddev=0.05))
+                weights_initializer=tf.truncated_normal_initializer(stddev=0.001))
         rcnn_boxes = slim.fully_connected(rcnn, num_classes*4, activation_fn=None, normalizer_fn=None, 
-                weights_initializer=tf.truncated_normal_initializer(stddev=0.05))
+                weights_initializer=tf.truncated_normal_initializer(stddev=0.001))
         rcnn_scores = tf.nn.softmax(rcnn_clses)
 
         ### decode rcnn network final outputs

train/test.py
@@ -117,26 +117,17 @@ def test():
                              FLAGS.dataset_split_name, 
                              FLAGS.dataset_dir, 
                              FLAGS.im_batch,
-                             is_training=True)
+                             is_training=False)
 
-    data_queue = tf.RandomShuffleQueue(capacity=32, min_after_dequeue=16,
-            dtypes=(
-                image.dtype, ih.dtype, iw.dtype, 
-                gt_boxes.dtype, gt_masks.dtype, 
-                num_instances.dtype, img_id.dtype)) 
-    enqueue_op = data_queue.enqueue((image, ih, iw, gt_boxes, gt_masks, num_instances, img_id))
-    data_queue_runner = tf.train.QueueRunner(data_queue, [enqueue_op] * 4)
-    tf.add_to_collection(tf.GraphKeys.QUEUE_RUNNERS, data_queue_runner)
-    (image, ih, iw, gt_boxes, gt_masks, num_instances, img_id) =  data_queue.dequeue()
     im_shape = tf.shape(image)
     image = tf.reshape(image, (im_shape[0], im_shape[1], im_shape[2], 3))
 
     ## network
     logits, end_points, pyramid_map = network.get_network(FLAGS.network, image,
-            weight_decay=FLAGS.weight_decay, is_training=True)
+            weight_decay=FLAGS.weight_decay, is_training=False)
     outputs = pyramid_network.build(end_points, im_shape[1], im_shape[2], pyramid_map,
             num_classes=81,
-            base_anchors=9,
+            base_anchors=15,
             is_training=False,
             gt_boxes=None, gt_masks=None, loss_weights=[0.0, 0.0, 0.0, 0.0, 0.0])
 